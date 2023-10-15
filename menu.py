a23cc00dcfa1458cf40ca2528adfdbbf15ae98bb
N-Rem <nicolasromero.barrios@gmail.com>
2023-10-14 21:59:02 -0300

    Aregla objetos y agrega submenus

--- menu.py
+++ menu.py
@@ -1,10 +1,58 @@
-#!!! -----menu------
+##!!! -----menu------
 
-op = input("1. Ingresar cómo alumno\n2. Ingresar cómo profesor\n3. Ver cursos\n4. Salir\n\t")
+op = input(
+    "1. Ingresar cómo alumno\n2. Ingresar cómo profesor\n3. Ver cursos\n4. Salir\n\t")
 
-if(op == str):
-    cosas: 0
-else: 
+if (op.isnumeric()):
+    op = int(op)
+    if (op == 1):
+        print("\n\t\t--Ingresar Alumno--")
+        if (True):
+            print("existe, da accedo al sistema.")
+            op_alumn = input("1. Matricularse a un curso\n2. Ver curso\n3. Volver al menú principal\n\t")
+            if (op_alumn.isnumeric()):
+                op_alumn = int(op_alumn)
+                if op_alumn == 1:
+                    "Lsita de cursos"
+                elif op_alumn == 2:
+                    "Lista de cursos alumno matriculado"
+                elif op_alumn == 3:
+                    "Vuelve al menu principal"
+                else:
+                    print("Opcion no valida")
+            else:
+                print("Debe ser un numero entero")
+        else:
+            print("Error de ingreso.")
+    elif (op == 2):
+        print("\n\t\t--Ingresar Profesor--")
+        if(True): 
+            print("existe, da accedo al sistema.")
+            op_prof = input("1. Dictar curso\n2. Ver curso\n3. Volver al menú principal\n\t")
+            if (op_prof.isnumeric()):
+                op_prof = int(op_prof)
+                if op_prof == 1:
+                    "se crea un curso y se la agrega a una Lsita de cursos"
+                elif op_prof == 2:
+                    "Lista de cursos Prof posee"
+                elif op_prof == 3:
+                    "Vuelve al menu principal"
+                else:
+                    print("Opcion no valida")
+            else:
+                print("Debe ser un numero entero")
+        else:
+            print("Error de ingreso.")
+
+    elif (op == 3):
+        print("\n\t\t--Ver Curso--")
+        "lista de los cursos."
+
+    elif (op == 4):
+        print("\n\t\t--Salir--")
+
+    else:
+        print("\n\tOpcion no valida.")
+
+else:
     print("Debe ser un numero entero.")
-
-
--- objetos.py
+++ objetos.py
@@ -1,11 +1,17 @@
+alumnos_registrados = []
+profesores_registrados = []
+cusos= []
+
 class Usuario():
-    def __init__ (self, nomb, lasname, mail, passw):
-        self.__nomb: nomb
-        self.__lasname: lasname
-        self.__mail: mail
-        self.__passw: passw
-    def __str__():
-        self =" cosas"
+    def __init__ (self, nombre, apellido, email, contrasenia):
+        self.__nombre: nombre 
+        self.__apellido: apellido
+        self.__email: email
+        self.__contrasenia: contrasenia
+    
+    def __str__(self):
+        return f"Usuario: Nombre = {self.__nombre}, Apellido = {self.__apellido}, E-mail = {self.__email}, Contraseña = {self.__contrasenia}"
+    
     def validar_credenciales(mail, passw): 
         if (mail != str or passw != str):
             return False
@@ -13,21 +19,57 @@ class Usuario():
 # !_____________________        
         
 class Estudiante(Usuario): 
-    def __init__(self, legajo):
-        self.legajo: legajo
+    def __init__ (self, nombre, apellido, email, contrasenia, legajo, anio_inscripcion_carrera):
+        Usuario.__init__(self, nombre, apellido, email, contrasenia)
+        self.__legajo: legajo
+        self.__anio_inscripcion_carrera: anio_inscripcion_carrera
+    
+    def __str__ (self):
+        return f"Usuario: Nombre = {self.__nombre}, Apellido = {self.__apellido}, E-mail = {self.__email}, Contraseña = {self.__contrasenia}, Legajo = {self.__legajo}, Año Inscripcion de la Carrera = {self.__anio_inscripcion_carrera}"
+    
+    def matricular_en_curso(curso):
+        pass        
+
+
 # !_____________________        
         
         
 class Profesor(Usuario): 
-    def __init__(self, nomb, lasname, mail, passw):
-        super().__init__(nomb, lasname, mail, passw, titulo, egreso)
+    def __init__ (self, nombre, apellido, email, contrasenia, titulo, anio_egreso):
+        Usuario.__init__(self, nombre, apellido, email, contrasenia)
+        self.__titulo: titulo
+        self.__anio_egreso: anio_egreso
+    
+    def __str__ (self):
+        return f"Usuario: Nombre = {self.__nombre}, Apellido = {self.__apellido}, E-mail = {self.__email}, Contraseña = {self.__contrasenia}, Titulo = {self.__titulo}, Año Egreso = {self.__anio_egreso}"
+        
+    
+    def dictar_curso (curos):
+        pass
+        
+
 # !_____________________        
 
 
 class Cusro (Estudiante, Profesor):
-    def __init__(self, nombr, pass_matriculacion):
-        self.nomb: nombr
+    def __init__(self, nombre, contrasenia_matriculacion):
+        self.__nombre: nombre
+        self.__contrasenia_matriculacion: contrasenia_matriculacion
+    
+    def __str__(self):
+        return f"Curso: Nombre = {self.__nombre}, Contraseña de Matriculacion = {self.__contrasenia_matriculacion}"    
+    
     def __generar_contrasenia__ ():
-        return "Contraseña"
+        passw = 0
+        return passw
 # !_____________________        
     
+def Buscar_alumno():
+    return str
+    pass
+
+def Buscar_existencia():
+    return boolean
+
+def agregar_a_lista(lista_de, ob):
+    lista_de.append(ob)
