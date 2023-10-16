8efc5dd6132a2369c1b4b34731d76b7b072af5f4
N-Rem <nicolasromero.barrios@gmail.com>
2023-10-16 17:44:33 -0300

    update del trabajo.

--- menu.py
+++ menu.py
@@ -10,12 +10,9 @@ while (op != 4):
             print("\n\t\t--Ingresar Alumno--\n\n")
             mail = input("Ingrese su E-mail: ")
             contrasenia = input("Ingrese su contraseña: ")
-            validacion, indice = ob.buscar_usuario(
-                ob.alumnos_registrados, mail)
+            validacion, indice = ob.buscar_usuario(ob.alumnos_registrados, mail)
             if (validacion):
-                print("\n\t\t--Accedio al Sistema.--\n")
-                op_alumn = input(
-                    "1. Matricularse a un curso\n2. Ver curso\n3. Volver al menú principal\n\t")
+                op_alumn = input("\n\t\t--Accedio al Sistema.--\n1. Matricularse a un curso\n2. Ver curso\n3. Volver al menú principal\n\t")
                 if (op_alumn.isdigit()):
                     op_alumn = int(op_alumn)
                     if op_alumn == 1:
@@ -24,10 +21,9 @@ while (op != 4):
                         if (op_alumn.isdigit()):
                             op_alumn = int(op_alumn)
                             if op_alumn <= len(ob.lista_cursos) and op_alumn >= 1:
-                                i_lista_curso = op_alumn - 1
+                                indice_curso = op_alumn - 1
                                 pass_curso = input("Digite la contraseña: ")
-                                ob.alumnos_registrados[indice].Matricular_en_curso(
-                                    ob.lista_cursos[i_lista_curso], pass_curso)
+                                ob.alumnos_registrados[indice].Matricular_en_curso(ob.lista_cursos[indice_curso], pass_curso)
                             else:
                                 print("Opcion no valida..")
                         else:
@@ -42,52 +38,40 @@ while (op != 4):
                     print("Debe ser un numero entero")
             else:
                 print("Error de ingreso.")
+        #!----------------Profesores-----------------------
         elif (op == 2):
             print("\n\t\t--Ingresar Profesor--\n\n")
             mail = input("Ingrese su E-mail: ")
             contrasenia = input("Ingrese su contraseña: ")
-            validacion, indice = ob.buscar_usuario(
-                ob.profesores_registrados, mail)
+            validacion, indice = ob.buscar_usuario(ob.profesores_registrados, mail)
             if (validacion):
-                prof = indice
-                print("\n\t\t--Accedio al Sistema.--\n")
-                op_prof = input(
-                    "1. Matricularse a un curso\n2. Ver curso\n3. Volver al menú principal\n\t")
+                op_prof = input("\n\t\t--Accedio al Sistema.--\n1. Dictar curso \n2. Ver mis cursos \n3. Volver al menú principal\n\t")
                 if (op_prof.isdigit()):
                     op_prof = int(op_prof)
                     if op_prof == 1:
-                        op_prof = input(
-                            "1 Programación I\n2 Programación II\n3 Laboratorio II\n4 InglesI\n5 InglesII...")
-                        if (op_prof.isdigit()):
-                            op_prof = int(op_prof)
-                            if op_prof == 1:
-                                pass
-                            elif op_prof == 2:
-                                pass
-                            elif op_prof == 3:
-                                pass
-                            elif op_prof == 4:
-                                pass
-                            elif op_prof == 5:
-                                pass
-                            else:
-                                print("Opcion no valida..")
-                        else:
-                            print("Debe ingresar un numero entero..")
+                        ob.profesores_registrados[indice].Dictar_curso()
+                        
                     elif op_prof == 2:
-                        ob.mostrar_cursos(
-                            ob.alumnos_registrados[prof].mis_cursos)
+                        ob.mostrar_mis_cursos(ob.profesores_registrados,indice)
+                        op_prof = input("Seleciones un curso.\n")
+                        if op_prof.isdigit():
+                            op_prof = int(op_prof)
+                            if (op_prof >= 1 or op_prof <= len(ob.profesores_registrados[indice].get_mis_cursos())):
+                                print(ob.profesores_registrados[indice].get_mis_cursos()[op_prof-1])
+                            else:
+                                print("Ingrese una opcion valida.. \n")
+                        else: 
+                            print("Debe ser un numero entero.. \n")
                     elif op_prof == 3:
-                        "Vuelve al menu principal"
+                        print("Vuelve al menu principal\n")
                     else:
-                        print("Opcion no valida")
+                        print("Opcion no valida\n")
                 else:
-                    print("Debe ser un numero entero")
+                    print("Debe ser un numero entero\n")
             else:
-                print("Error de ingreso.")
+                print("Error de ingreso.\n")
 
-        #!------Op Tres, lsito-----
-
+        #!------Op Tres-----
         elif (op == 3):
             print("\n\t\t--Ver Curso--")
             ob.mostrar_cursos(ob.lista_cursos)
@@ -96,6 +80,6 @@ while (op != 4):
             print("\n\t\t--Salir--")
 
         else:
-            print("\n\tOpcion no valida.")
+            print("\n\tOpcion no valida.\n")
     else:
-        print("Debe ser un numero entero.")
+        print("Debe ser un numero entero.\n")
--- objetos.py
+++ objetos.py
@@ -2,7 +2,6 @@ alumnos_registrados = []
 profesores_registrados = []
 lista_cursos = []
 
-
 class Usuario():
     def __init__(self, nombre, apellido, email, contrasenia):
         self.__nombre = nombre
@@ -10,76 +9,71 @@ class Usuario():
         self.__email = email
         self.__contrasenia = contrasenia
     
-    #!get-set Nombre---------
+    #!get-set--------->
     def get_nombre(self):
         return self.__nombre
     def set_nombre(self, nombre=str):
         self.__nombre = nombre
-    #!get-set Apellido-----------  
+    
     def get_apellido(self):
         return self.__apellido
     def set_apellido(self, apellido=str):
         self.__apellido = apellido
-    #!get-set email
+    
     def get_email(self):
         return self.__email
     def set_email(self, email=str):
         self.__email = email    
-             
+     #!<-----------------------        
     def __str__(self):
         return f"Nombre = {self.__nombre}, Apellido = {self.__apellido}, E-mail = {self.__email}, Contraseña = {self.__contrasenia}"
 
-    def validar_credenciales(self):
+    def validar_credenciales(self): #!!!isinstance(variable, tipo de dato): bool 
         if isinstance(self.__email, str) and isinstance(self.__contrasenia, str):
-            if not (self.__email.isdigit()) and not (self.__contrasenia.isdigit()):
+            if not (self.__email.isdigit()) and not (self.__contrasenia.isdigit()): #!!isdigit():bool
                 return True
             else:
                 return False
         else:
             return False
 # !_____________________
-
 class Estudiante(Usuario):
-    def __init__(self, nombre=str, apellido=str, email=str, contrasenia=str, legajo=int, anio_inscripcion_carrera=int, mis_cursos=[]):
+    def __init__(self, nombre=str, apellido=str, email=str, contrasenia=str, legajo=int, anio_inscripcion_carrera=int):
         super().__init__(nombre, apellido, email, contrasenia)
         self.__legajo = legajo
         self.__anio_inscripcion_carrera = anio_inscripcion_carrera
-        self.__mis_cursos = mis_cursos
-    #!get-set lista mis_cursos
+        self.__mis_cursos =[]
+    #!get-set ---------------->
     def get_mis_cursos(self):
         return self.__mis_cursos
     def set_mis_cursos(self, curso=object):
         self.__mis_cursos.append(curso)
+    #!<----------------------
     def __str__(self):
         return "Estudiante: " + super().__str__() + f", Legajo = {self.__legajo}, Año Inscripcion en la Carrera = {self.__anio_inscripcion_carrera}"
 
-    def Matricular_en_curso(self, curso: object, contrasenia=str, ):
-        if not(busca_curso(self.get_mis_cursos(), curso.get_nombre())):
+    def Matricular_en_curso(self, curso: object, contrasenia=str):
+        if (buscar_curso(curso.get_nombre(), self.__mis_cursos)):
+            print(f"Usted ya estaba anotado en {curso.get_nombre()}")
+        else:
             if (contrasenia == curso.get_contrasenia()):
                 self.__mis_cursos.append(curso)
-                print(f"Usted de Matriculo en {curso.get_nombre()}")
+                print(f"Materia: {curso.get_nombre()} agregada a Mis Cursos..")
             else:
-                print("Contraseña no valida..")
-        else:
-            print(f"Usted ya esta anotado en {curso.get_nombre()}")
-
+                print("Contraseña no valida..")       
 #--
-def busca_curso(lista=list, nombre=str):
-    
+def buscar_curso(curso=str, lista=list):
     for i in lista:
-        if i.__nombre == nombre:
+        if curso == i.get_nombre():
             return True
-        else:
-            return False
-
+    return False
 # !_____________________
-
 class Profesor(Usuario):
-    def __init__(self, nombre=str, apellido=str, email=str, contrasenia=str, titulo=str, anio_egreso=int, mis_cursos=[]):
+    def __init__(self, nombre=str, apellido=str, email=str, contrasenia=str, titulo=str, anio_egreso=int):
         super().__init__(nombre, apellido, email, contrasenia)
         self.__titulo = titulo
         self.__anio_egreso = anio_egreso
-        self.__mis_cursos = mis_cursos
+        self.__mis_cursos = []
 
     def get_mis_cursos(self):
         return self.__mis_cursos
@@ -96,9 +90,8 @@ class Profesor(Usuario):
         lista_cursos.append(nuevo_curso)
         print(nuevo_curso)
 # !_____________________
-
-class Curso (Estudiante, Profesor):
-    def __init__(self, nombre=str, contrasenia_matriculacion = str, archivos=list):
+class Curso ():
+    def __init__(self, nombre=str, archivos=list):
         self.__nombre = nombre
         self.__contrasenia_matriculacion = self.__generar_contrasenia()
         self.__archivos = archivos
@@ -122,8 +115,7 @@ class Curso (Estudiante, Profesor):
 
     def __generar_contrasenia(self):
         contracenia = "000"
-        return  contracenia
-        
+        return  contracenia      
 # !__________funciones___________
 def mostrar_listas(lista = list):
     for i,valor in enumerate(lista):
@@ -132,7 +124,7 @@ def mostrar_listas(lista = list):
 def mostrar_mis_cursos(lista_objetos = list, indice = int):
     mis_cursos = lista_objetos[indice].get_mis_cursos()
     mostrar_listas(mis_cursos)
-
+        
 def buscar_usuario(lista = list, email = str):
     for indice, valor in enumerate(lista): 
         objeto_lista = valor.get_email()
@@ -144,21 +136,14 @@ def mostrar_cursos(lista = list):
     for i in lista: 
         print(f"Materia: {i.get_nombre()}\t\tCarrera: Tecnicatura Universitaria en Programación\n")
 
-def agregar_a_lista(lista_de, ob):
-    lista_de.append(ob)
-
-
 #!---------Objetos-----------
-print("\n")
-
 alumno_uno = Estudiante("Lautaro", "Vega", "Lautaro.vega@gmail.com", "2424a", 5858, 2023)
-alumno_dos = Estudiante("Maria", "Perez", "Maria.perez@gmail.com", "1234a", 5050, 2022)
-alumno_tres = Estudiante("Victoria", "Garcia", "Vicky.gar@gmail.com", "2444a", 3030, 2023)
+alumno_dos = Estudiante("Maria", "Gomez", "Maria.g@gmail.com", "1234a", 5050, 2022)
+alumno_tres = Estudiante("Victoria", "Sosa", "Vicky.ss@gmail.com", "2444a", 3030, 2023)
 alumnos_registrados.append(alumno_uno)
 alumnos_registrados.append(alumno_dos)
 alumnos_registrados.append(alumno_tres)
 print(alumno_tres.get_email())
-
 
 prof_uno = Profesor("Gustavo", "Ramirez","Gustavo.Ramirez@gmail.com", "3883a", "Ingeniero", 2005)
 prof_dos = Profesor("Bettiana", "apellidoRaro","bettiana.123@gmail.com", "1010a", "Ingeniera", 2000)
@@ -167,11 +152,25 @@ profesores_registrados.append(prof_tres)
 profesores_registrados.append(prof_dos)
 profesores_registrados.append(prof_uno)
 
-curso_uno = Curso("matematicas")
-curso_dos = Curso("Programacion")
+curso_uno = Curso("Programacion I")
+curso_dos = Curso("Matematicas")
 curso_tres = Curso("Estadistica")
 lista_cursos.append(curso_uno)
 lista_cursos.append(curso_dos)
 lista_cursos.append(curso_tres)
 
+prof_uno.set_mis_cursos(curso_uno)
+prof_dos.set_mis_cursos(curso_dos)
+prof_tres.set_mis_cursos(curso_tres)
+
+alumno_tres.set_mis_cursos(curso_uno)
+alumno_tres.set_mis_cursos(curso_dos)
+alumno_tres.set_mis_cursos(curso_tres)
+
 print("\n")
+
+mostrar_mis_cursos(alumnos_registrados, 0)
+mostrar_mis_cursos(alumnos_registrados,1)
+mostrar_mis_cursos(alumnos_registrados,2)
+
+mostrar_mis_cursos(profesores_registrados,2)
